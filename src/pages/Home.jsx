742b396c8b64e1694dbd4cc2275baf57f1b5df27
Supak Kovitprapas <lin@scoutout.net>
2021-11-19 15:48:47 +0700

    add react router

---
+++ src/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Home = (props) => (
+  <div className="HomeWrapper">
+    Home content
+  </div>
+);
+
+export default Home;
---
+++ src/pages/PlayBoard.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PlayBoard = (props) => (
+  <div className="PlayBoardWrapper">
+    play-board content
+  </div>
+);
+
+export default PlayBoard;
