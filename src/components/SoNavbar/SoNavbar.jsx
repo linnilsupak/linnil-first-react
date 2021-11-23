b6cc752eb5848813930240c0b87da4738d3e2dc1
Supak Kovitprapas <lin@scoutout.net>
2021-11-23 13:57:24 +0700

    show xo icon

--- src/components/SoNavbar/SoNavbar.jsx
+++ src/components/SoNavbar/SoNavbar.jsx
@@ -3,12 +3,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import { Link } from "react-router-dom";
+import './SoNavbar.styles.scss';
 import PropTypes from 'prop-types';
 
+const icon = require('../../assets/img/chibi_cat_icon.jpeg');
 const SoNavbar = (props) => (
   <Navbar bg="dark" variant="dark">
     <Container>
-      <Navbar.Brand as={Link} to="/">Linnil XO</Navbar.Brand>
+      <Navbar.Brand as={Link} to="/">
+        <img src={String(icon.default)} alt="linnil icon"
+          className="d-inline-block align-top circle"/>
+        &nbsp;Linnil XO
+      </Navbar.Brand>
       <Nav className="me-auto">
         <Nav.Link as={Link} to="home">Home</Nav.Link>
         <Nav.Link as={Link} to="playground">Playground</Nav.Link>
